9a59088941fd1b6175af341930f876d0263019fa
arulebin <ebinarul@protonmail.com>
2023-12-25 21:14:19 +0530

    Add more extensions and some minor change

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
 import shutil
 
-file_types={'Documents':'pdf','Music':'flac','Videos':'mkv','Pictures':'jpg'}
+file_types={
+        'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp'],
+        'Documents': ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'],
+        'Videos': ['.mp4', '.avi', '.mkv', '.mov'],
+        'Music': ['.mp3', '.wav', '.flac'],
+        'Archives': ['.zip', '.rar', '.tar', '.gz'],
+        'Others':[]
+    }
 
 src_dir=os.getcwd()
-files=os.listdir(src_dir)
 
-for file in files:
-    for folder,ext in file_types.items():
-        if file.endswith(ext):
+for file in os.listdir(src_dir):
+    _, file_ext=os.path.splitext(file) 
+    
+    for folder,ext in file_types.items():  
+        if file_ext in ext:
             os.mkdir(folder)
             folder_path=os.path.join(src_dir,folder)
             shutil.move(f"{file}",folder_path)
